4f7b360baab163bf3464c50fc37fa24eb2714af0
lucas <lucas.sebib@egmail.com>
2022-11-29 19:54:57 -0300

    creamos aplicacion para productos

--- catalogo/catalogo/views.py
+++ catalogo/catalogo/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+from productos.models import Producto
+
 def inicio(request):
     template_name = "index.html"
+    
+    # ===============================
+    # query en django utilizando el orm
+    """
+    p = Producto.objects.create(
+        nombre="Pantalon",
+        precio=2000,
+        descripcion="Pantalon azul"
+    )
+    """
+    productos = Producto.objects.all()
+
     contexto = {
+        'productos': productos
     }
     return render(request, template_name, contexto)
---
+++ catalogo/productos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
